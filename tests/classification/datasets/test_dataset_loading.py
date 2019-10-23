f4625a4ad23530588485cd926aa6d1d89bb29597
Antoine Hebert <hantoine02@gmail.com>
2019-10-23 16:36:38 -0400

    Fix tests not testing download of dataset

--- tests/classification/datasets/test_dataset_loading.py
+++ tests/classification/datasets/test_dataset_loading.py
@@ -1,4 +1,10 @@
 import classification.datasets as ds
+from shutil import rmtree
+from os.path import isdir
+
+ds.workdir = 'test-workdir'
+if isdir(ds.workdir):
+    rmtree(ds.workdir)
 
 def test_default_credit_card_dataset_loading():
     ds.DefaultCreditCardDataset.get()
