d7d39564818412c230f0e0b49791a8beeb904d62
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-10-23 17:15:19 -0400

    SteelPlates and SeismicBumps dataset

--- classification/datasets/__init__.py
+++ classification/datasets/__init__.py
@@ -4,8 +4,11 @@ from os import makedirs
 import urllib.request
 import urllib.parse
 import pandas as pd
+from scipy.io import arff
+import numpy as np
 
 workdir = os.path.join(os.getcwd(), 'data')
+
 
 class Dataset:
     @classmethod
@@ -24,16 +27,18 @@ class Dataset:
         url = url.replace(' ', '%20')
         urllib.request.urlretrieve(url, os.path.join(workdir, filename))
 
+
 class DefaultCreditCardDataset(Dataset):
     filename = 'default of credit card clients.xls'
     url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default of credit card clients.xls'
+
     @classmethod
     def get(cls):
         dataset_path = os.path.join(workdir, cls.filename)
         if not isfile(dataset_path):
             cls.download()
         df = pd.read_excel(dataset_path, header=[0, 1])
-        
+
         y = df['Y'].values.ravel()
         X = df[[f'X{i}' for i in range(1, 24)]].values
 
@@ -42,8 +47,50 @@ class DefaultCreditCardDataset(Dataset):
 
         return X, y
 
+
+class SteelPlatesFaultsDataset(Dataset):
+    urls = ['https://archive.ics.uci.edu/ml/machine-learning-databases/00198/Faults.NNA',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/00198/Faults27x7_var']
+    filenames = ['Faults.NNA', 'Faults27x7_var']
+
+    @classmethod
+    def get(cls):
+        dataset_path = os.path.join(workdir, cls.filenames[0])
+        if not isfile(dataset_path):
+            cls.download()
+
+        df = pd.read_csv(dataset_path, sep='\t', header=None)
+
+        with open(os.path.join(workdir, cls.filenames[1]), 'r') as f:
+            cls.feature_names = f.read().strip().split('\n')
+
+        return df
+
+
+class SeismicBumps(Dataset):
+    filename = 'seismic-bumps.arff'
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00266/seismic-bumps.arff'
+
+    @classmethod
+    def get(cls):
+        dataset_path = os.path.join(workdir, cls.filename)
+        if not isfile(dataset_path):
+            cls.download()
+
+        data, meta = arff.loadarff(dataset_path)
+
+        df = pd.DataFrame(data)
+        df['class'] = pd.to_numeric(df['class'])
+
+        str_df = df.select_dtypes([np.object])
+        str_df = str_df.stack().str.decode('utf-8').unstack()
+        for col in str_df:
+            df[col] = str_df[col]
+
+        return df
+
+
 # Usage example
 if __name__ == '__main__':
     X, y = DefaultCreditCardDataset.get()
     print(f'Features: {DefaultCreditCardDataset.feature_names}')
-
