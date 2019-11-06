58e3ef9a2125119e4400c954c5f57461b7ca0f7b
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-11-06 14:37:47 -0500

    Refactoring

--- regression/datasets/loaders.py
+++ regression/datasets/loaders.py
@@ -14,75 +14,6 @@ from utils import Dataset, test_size, random_state
 from config import DEFAULT_DATA_DIR
 
 
-class ParkinsonMultipleSoundRecordingDataset(Dataset):
-    filename = 'Parkinson_Multiple_Sound_Recording.rar'
-    filenames = ['train_data.txt',
-                 'test_data.txt']
-
-    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00301/Parkinson_Multiple_Sound_Recording.rar'
-
-    feature_names = ['subject_id',
-                     'jitter_local', 'jitter_local_abs', 'jitter_rap',
-                     'jitter_ppq5', 'jitter_ddp', 'shimmer_local',
-                     'shimmer_local_db', 'shimmer_apq3', 'shimmer_apq5',
-                     'shimmer_apq11', 'shimmer_dda', 'AC', 'NTH', 'HTN',
-                     'median_pitch', 'mean_pitch', 'std', 'minimum_pitch',
-                     'maximum_pitch', 'nb_pulses', 'nb_periods', 'mean_period',
-                     'std', 'frac_local_unvoiced_frames', 'nb_voice_breaks',
-                     'degree_voice_breaks']
-
-    @classmethod
-    def parse_dataset(cls, df):
-        X, y = df[df.columns[:27]], df[df.columns[26]]
-        X.columns = cls.feature_names
-        return X, y
-
-    @classmethod
-    def get(cls, workdir=DEFAULT_DATA_DIR):
-        dataset_path = os.path.join(workdir, cls.filename)
-        if not isfile(dataset_path):
-            cls.download(workdir)
-        patoolib.extract_archive(dataset_path, outdir=workdir)
-        time.sleep(2)
-
-        train_df = cls.get_df(workdir, cls.filenames[0])
-        test_df = cls.get_df(workdir, cls.filenames[1])
-        train_df = train_df.drop([27])
-
-        X_train, y_train = cls.parse_dataset(train_df)
-        X_test, y_test = cls.parse_dataset(test_df)
-        return (X_train, y_train), (X_test, y_test)
-
-
-class MerckMolecularActivityChallengeDataset(Dataset):
-    filename = 'MerckActivity.zip'
-    url = 'https://www.kaggle.com/c/2975/download-all/'
-
-    @classmethod
-    def get(cls, workdir=DEFAULT_DATA_DIR):
-        dataset_path = os.path.join(workdir, cls.filename)
-        if not isfile(dataset_path):
-            cls.download(workdir)
-        with ZipFile(dataset_path, 'r') as zipfile:
-            zipfile.extractall(workdir)
-        return
-
-
-class QsarAquaticToxicityDataset(Dataset):
-    filename = 'qsar_aquatic_toxicity.csv'
-    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00505//qsar_aquatic_toxicity.csv'
-    features = ['TPSA', 'SAacc', 'H-050', 'MLOGP', 'RDCHI', 'GATS1p', 'nN', 'C-040', 'LC50']
-
-    @classmethod
-    def get(cls, workdir=DEFAULT_DATA_DIR):
-        df = cls.get_df(workdir, cls.filename)
-        df.columns = cls.features
-        y = df[df.columns[-1]]
-        X = df[df.columns[:-1]]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-        return (X_train, y_train), (X_test, y_test)
-
-
 class WhiteWineQualityDataset(Dataset):
     filename = 'winequality-red.csv'
     url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv'
@@ -131,20 +62,58 @@ class CommunitiesAndCrimeDataset(Dataset):
         return (X_train, y_train), (X_test, y_test)
 
 
-class ConcreteCompressiveStrengthDataset(Dataset):
-    filename = 'Concrete_Data.xls'
-    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls'
+class QsarAquaticToxicityDataset(Dataset):
+    filename = 'qsar_aquatic_toxicity.csv'
+    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00505//qsar_aquatic_toxicity.csv'
+    features = ['TPSA', 'SAacc', 'H-050', 'MLOGP', 'RDCHI', 'GATS1p', 'nN', 'C-040', 'LC50']
+
+    @classmethod
+    def get(cls, workdir=DEFAULT_DATA_DIR):
+        df = cls.get_df(workdir, cls.filename)
+        df.columns = cls.features
+        y = df[df.columns[-1]]
+        X = df[df.columns[:-1]]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+        return (X_train, y_train), (X_test, y_test)
+
+
+class ParkinsonMultipleSoundRecordingDataset(Dataset):
+    filename = 'Parkinson_Multiple_Sound_Recording.rar'
+    filenames = ['train_data.txt',
+                 'test_data.txt']
+
+    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00301/Parkinson_Multiple_Sound_Recording.rar'
+
+    feature_names = ['subject_id',
+                     'jitter_local', 'jitter_local_abs', 'jitter_rap',
+                     'jitter_ppq5', 'jitter_ddp', 'shimmer_local',
+                     'shimmer_local_db', 'shimmer_apq3', 'shimmer_apq5',
+                     'shimmer_apq11', 'shimmer_dda', 'AC', 'NTH', 'HTN',
+                     'median_pitch', 'mean_pitch', 'std', 'minimum_pitch',
+                     'maximum_pitch', 'nb_pulses', 'nb_periods', 'mean_period',
+                     'std', 'frac_local_unvoiced_frames', 'nb_voice_breaks',
+                     'degree_voice_breaks']
+
+    @classmethod
+    def parse_dataset(cls, df):
+        X, y = df[df.columns[:27]], df[df.columns[26]]
+        X.columns = cls.feature_names
+        return X, y
 
     @classmethod
     def get(cls, workdir=DEFAULT_DATA_DIR):
         dataset_path = os.path.join(workdir, cls.filename)
         if not isfile(dataset_path):
             cls.download(workdir)
-        df = pd.read_excel(dataset_path)
-        print(df)
-        X = df[df.columns[:-1]]
-        y = df[df.columns[-1]]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        patoolib.extract_archive(dataset_path, outdir=workdir)
+        time.sleep(2)
+
+        train_df = cls.get_df(workdir, cls.filenames[0])
+        test_df = cls.get_df(workdir, cls.filenames[1])
+        train_df = train_df.drop([27])
+
+        X_train, y_train = cls.parse_dataset(train_df)
+        X_test, y_test = cls.parse_dataset(test_df)
         return (X_train, y_train), (X_test, y_test)
 
 
@@ -160,8 +129,22 @@ class BikeSharingDataset(Dataset):
         with ZipFile(dataset_path, 'r') as zipfile:
             zipfile.extractall(workdir)
         df = pd.read_csv(os.path.join(workdir, 'hour.csv'), sep=',')
-        print(df)
+        X = df[df.columns[:-1]]
+        y = df[df.columns[-1]]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        return (X_train, y_train), (X_test, y_test)
 
+
+class ConcreteCompressiveStrengthDataset(Dataset):
+    filename = 'Concrete_Data.xls'
+    url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls'
+
+    @classmethod
+    def get(cls, workdir=DEFAULT_DATA_DIR):
+        dataset_path = os.path.join(workdir, cls.filename)
+        if not isfile(dataset_path):
+            cls.download(workdir)
+        df = pd.read_excel(dataset_path)
         X = df[df.columns[:-1]]
         y = df[df.columns[-1]]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
