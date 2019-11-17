b208e119c9cb73bffd680db08273ea18bc56b49d
Antoine Hebert <hantoine02@gmail.com>
2019-11-17 18:14:24 -0500

    Improve tuning scripts: tuning for a constant time for all models (as opposed to contant number of trials)

--- classification/main.py
+++ classification/main.py
@@ -1,7 +1,10 @@
-#%% Get Dataset
+from copy import deepcopy
+import warnings
+import time
 import numpy as np
 from sklearn.metrics import confusion_matrix as compute_confusion_matrix
 from sklearn.model_selection import StratifiedKFold
+from sklearn.exceptions import ConvergenceWarning
 from hyperopt import Trials, fmin, tpe, space_eval
 
 from classification import datasets as ds
@@ -9,8 +12,11 @@ from classification import models
 from classification.metrics import compute_loss
 from utils import random_state
 
-HP_TUNING_TRIALS = 30
+HP_TUNING_STEP_TRIALS = 5
+HP_TUNE_MAX_TIME = 120
 K_FOLD_K_VALUE = 7
+
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 def get_objective(dataset, model, train, kfold):
     def objective(args):
@@ -30,21 +36,39 @@ def get_objective(dataset, model, train, kfold):
 
     return objective
 
+def tune_hyperparams(trials, dataset, model, train_data, kfold, rstate):
+    return fmin(get_objective(dataset, model, train_data, kfold),
+                model.hp_space,
+                algo=tpe.suggest,
+                max_evals=len(trials.trials) + HP_TUNING_STEP_TRIALS,
+                trials=trials,
+                show_progressbar=True,
+                rstate=rstate)
+
 for dataset in ds.all_datasets:
     print(f'Dataset: {dataset.__name__}')
     train, test = dataset.get()
     kfold = StratifiedKFold(n_splits=min(K_FOLD_K_VALUE, ds.get_min_k_fold_k_value(train)),
                             shuffle=True, random_state=random_state)
     for model in models.all_models:
-        train, test = model.prepare_dataset(train, test, dataset.categorical_features)
+        print(f'Model: {model.__name__}')
+
+        # Pre-processings are only applied on the data used by this model
+        train_prepared, test_prepared = deepcopy((train, test))
+        train_prepared, test_prepared = \
+            model.prepare_dataset(train_prepared, test_prepared, dataset.categorical_features)
+
         trials = Trials()
-        best = fmin(get_objective(dataset, model, train, kfold),
-                    model.hp_space,
-                    algo=tpe.suggest,
-                    max_evals=HP_TUNING_TRIALS,
-                    trials=trials,
-                    show_progressbar=True,
-                    rstate=np.random.RandomState(random_state))
+        rstate = np.random.RandomState(random_state)
+        start_time = time.time()
+        while time.time() - start_time < HP_TUNE_MAX_TIME:
+            best = tune_hyperparams(trials, dataset, model, train_prepared, kfold, rstate)
+        tuning_time = time.time() - start_time
+
         best_score = -min(trials.losses())
         best_hp = space_eval(model.hp_space, best)
-        print(f'Best {dataset.metric}: {best_score}\nWith hyperparams: {best_hp}')
+        best_trial_index = np.array(trials.losses()).argmin()
+        print(f'Best {dataset.metric}: {best_score}')
+        print(f'With hyperparams: \n{best_hp}')
+        print(f'Obtained after {best_trial_index-1} trials')
+        print(f'Total tuning time: {tuning_time:.0f}s')
--- regression/main.py
+++ regression/main.py
@@ -1,6 +1,10 @@
+from copy import deepcopy
+import warnings
+import time
 import numpy as np
 from sklearn.model_selection import KFold, GroupKFold
 from sklearn.utils import shuffle
+from sklearn.exceptions import ConvergenceWarning
 from hyperopt import Trials, fmin, tpe, space_eval
 
 from regression import datasets as ds
@@ -8,8 +12,11 @@ from regression import models
 from regression.metrics import compute_metric, aggregate_metrics
 from utils import random_state
 
-HP_TUNING_TRIALS = 50
+HP_TUNING_STEP_TRIALS = 5
+HP_TUNE_MAX_TIME = 120
 K_FOLD_K_VALUE = 7
+
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 def get_objective(dataset, model, train, kfold):
     def objective(args):
@@ -29,6 +36,15 @@ def get_objective(dataset, model, train, kfold):
 
     return objective
 
+def tune_hyperparams(trials, dataset, model, train_data, kfold, rstate):
+    return fmin(get_objective(dataset, model, train_data, kfold),
+                model.hp_space,
+                algo=tpe.suggest,
+                max_evals=len(trials.trials) + HP_TUNING_STEP_TRIALS,
+                trials=trials,
+                show_progressbar=True,
+                rstate=rstate)
+
 for dataset in ds.all_datasets:
     print(f'Dataset: {dataset.__name__}')
     train, test = dataset.get()
@@ -38,18 +54,23 @@ for dataset in ds.all_datasets:
     else:
         kfold = KFold(n_splits=K_FOLD_K_VALUE, shuffle=True, random_state=random_state)
     for model in models.all_models:
-        train, test = model.prepare_dataset(train, test, dataset.categorical_features)
+        print(f'Model: {model.__name__}')
+        # Pre-processings are only applied on the data used by this model
+        train_prepared, test_prepared = deepcopy((train, test))
+        train_prepared, test_prepared = \
+            model.prepare_dataset(train_prepared, test_prepared, dataset.categorical_features)
+
         trials = Trials()
-        best = fmin(get_objective(dataset, model, train, kfold),
-                    model.hp_space,
-                    algo=tpe.suggest,
-                    max_evals=HP_TUNING_TRIALS,
-                    trials=trials,
-                    show_progressbar=True,
-                    rstate=np.random.RandomState(random_state))
+        rstate = np.random.RandomState(random_state)
+        start_time = time.time()
+        while time.time() - start_time < HP_TUNE_MAX_TIME:
+            best = tune_hyperparams(trials, dataset, model, train_prepared, kfold, rstate)
+        tuning_time = time.time() - start_time
+
         best_score = -min(trials.losses())
         best_hp = space_eval(model.hp_space, best)
         best_trial_index = np.array(trials.losses()).argmin()
         print(f'Best {dataset.metric}: {best_score}')
-        print(f'With hyperparams: {best_hp}')
+        print(f'With hyperparams: \n{best_hp}')
         print(f'Obtained after {best_trial_index-1} trials')
+        print(f'Total tuning time: {tuning_time:.0f}s')
