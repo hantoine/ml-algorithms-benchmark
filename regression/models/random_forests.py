3ea4bcfd9cf8c692ee7977d512a5c91266fe1da5
Jean Lucas <12112853+jeanlst@users.noreply.github.com>
2019-11-19 15:32:39 -0500

    Tree Based Models

--- regression/models/random_forests.py
+++ regression/models/random_forests.py
@@ -1,37 +1,15 @@
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-import pandas as pd
-from utils import CategoryEncoder, random_state
+from utils import random_state, TreeBasedModel
 from hyperopt import hp
 from hyperopt.pyll import scope
 import numpy as np
 
-class RandomForestsModel:
-    @staticmethod
-    def prepare_dataset(train_data, test_data, categorical_features):
-        X_train, y_train, *other = train_data
-        X_test, y_test = test_data
-        ce = CategoryEncoder(categorical_features, method='onehot')
-        X_train_enc = ce.fit_transform(X_train, y_train)
-        X_test_enc = ce.transform(X_test)
 
-        # if Xs are numpy array, ys needs to be too (for k-fold)
-        if type(y_train) == pd.Series:
-            y_train, y_test = y_train.values, y_test.values
-
-        # Impute missing values if any
-        if np.isnan(X_train_enc).any() or np.isnan(X_test_enc).any():
-            imp = IterativeImputer(max_iter=10, random_state=random_state)
-            X_train_enc = imp.fit_transform(X_train_enc)
-            X_test_enc = imp.transform(X_test_enc)
-
-        return (X_train_enc, y_train, *other), (X_test_enc, y_test)
-
+class RandomForestsModel(TreeBasedModel):
     @staticmethod
     def build_estimator(args):
         return RandomForestRegressor(random_state=random_state, n_jobs=-1, **args)
-    
+
     hp_space = {
         'max_depth': hp.pchoice('max_depth_enabled', [
             (0.7, None),
@@ -48,4 +26,3 @@ class RandomForestsModel:
             (0.6, hp.uniform('max_features_str_frac', 0., 1.))
         ])
     }
-
---
+++ utils/models.py
@@ -0,0 +1,43 @@
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from utils import CategoryEncoder, random_state
+import numpy as np
+import scipy
+
+
+class TreeBasedModel:
+    @classmethod
+    def prepare_dataset(cls, train_data, test_data, categorical_features):
+        X_train, y_train, *other = train_data
+        X_test, y_test = test_data
+        is_binary_classification = len(np.unique(y_train)) == 2
+        if is_binary_classification:
+            ce = CategoryEncoder(categorical_features, method='sorted_ordinal')
+        else:
+            ce = CategoryEncoder(categorical_features, method='onehot')
+        X_train_enc = ce.fit_transform(X_train, y_train)
+        X_test_enc = ce.transform(X_test)
+
+        # if Xs are numpy array, ys needs to be too (for k-fold)
+        if type(y_train) == pd.Series:
+            y_train, y_test = y_train.values, y_test.values
+
+        # Impute missing values if any
+        if np.isnan(X_train_enc).any() or np.isnan(X_test_enc).any():
+            # StandardScaler does not support sparse matrix
+            if type(X_train_enc) == scipy.sparse.csr.csr_matrix:
+                X_train_enc = X_train_enc.toarray()
+                X_test_enc = X_test_enc.toarray()
+
+            # Scaling first since imputer uses BayesianRidge
+            scaler = StandardScaler()
+            X_train_enc = scaler.fit_transform(X_train_enc)
+            X_test_enc = scaler.transform(X_test_enc)
+
+            imp = IterativeImputer(max_iter=10, random_state=random_state)
+            X_train_enc = imp.fit_transform(X_train_enc)
+            X_test_enc = imp.transform(X_test_enc)
+
+        return (X_train_enc, y_train, *other), (X_test_enc, y_test)
