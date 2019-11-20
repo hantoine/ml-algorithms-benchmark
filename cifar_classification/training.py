0c17d7220abc1004935a1ea1e458f41e93ec4e9d
GTimothee <ava_punksmash@protonmail.com>
2019-11-20 16:22:50 -0500

    added training function

--- cifar_classification/training.py
+++ cifar_classification/training.py
@@ -1,24 +1,35 @@
 import torch
+import torchvision
 import torch.nn as nn
+import torch.optim as optim
+import cifar_classifiation as cs 
+from cs.cifar_utilities import get_loaders
 
-class LeNet5(nn.Module):
-    """ baseline
-    using average pooling and tanh as activation function
-    """
-    def __init__(self):
-        super(LeNet5, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.AvgPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+def training(trainloader, net, config):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=config.lr, momentum=config.momentum)
 
-    def forward(self, x):
-        x = self.pool(torch.tanh(self.conv1(x)))
-        x = self.pool(torch.tanh(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    net.train()
+    for epoch in range(config.nb_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            
+        print(f'Epoch {epoch} loss: {running_loss / i}')
+        running_loss = 0.0
+
+    print('Finished Training')
