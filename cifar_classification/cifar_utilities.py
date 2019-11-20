ab56da6e10e63caf03ce7f99bf46bf409f5f7a3d
GTimothee <ava_punksmash@protonmail.com>
2019-11-19 21:16:07 -0500

    added get_data_from_files

--- cifar_classification/cifar_utilities.py
+++ cifar_classification/cifar_utilities.py
@@ -21,4 +21,25 @@ def get_data(filename):
     batch = unpickle(filename)
     return batch[b'data'], batch[b'labels']
 
-    
+
+def get_data_from_files(data_path):
+    os.chdir(data_path)
+    labels_info = dict()
+    files = [f for f in glob.glob("*")]
+    train_files = [f for f in glob.glob("data_batch*")]
+
+    for f in files:
+        if f == "batches.meta":
+            print(f'file {f} processed')
+            labels_info = unpickle(f)
+        elif f == "test_batch":
+            X_test, y_test = get_data(f)
+
+    X, Y = get_data(train_files[0])
+    for f in train_files[1:]:
+        print(f'file {f} processed')
+        x, y = get_data(f)
+        X = np.concatenate((X, x))
+        Y = np.concatenate((Y, y))
+
+    return (X,Y), (X_test, y_test), labels_info
