a319be26cb77f9df94865812931d32e97948167d
GTimothee <ava_punksmash@protonmail.com>
2019-11-19 21:15:12 -0500

    added unpickle function

--- cifar_classification/cifar_utilities.py
+++ cifar_classification/cifar_utilities.py
@@ -2,3 +2,14 @@ import glob, os
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+def unpickle(file_path):
+    """ Function from documentation
+    see: https://www.cs.toronto.edu/~kriz/cifar.html
+    return:
+        d: dictionary containing the data
+    """
+    with open(file_path, 'rb') as fo:
+        d = pickle.load(fo, encoding='bytes')
+    return d
