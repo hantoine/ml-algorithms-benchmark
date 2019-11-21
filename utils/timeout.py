3cd343781dc77839436d8d46feca0b8996ab19e1
Antoine Hébert <hantoine02@gmail.com>
2019-11-20 22:49:22 -0500

    Remove no longer used arguments of set_timeout

--- utils/timeout.py
+++ utils/timeout.py
@@ -1,5 +1,5 @@
 from timeout_decorator import timeout
 from timeout_decorator.timeout_decorator import TimeoutError
 
-def set_timeout(fct, timeout_time, use_signals=True, exception_message=None):
-    return timeout(timeout_time, use_signals, exception_message=exception_message)(fct)
+def set_timeout(fct, timeout_time):
+    return timeout(timeout_time)(fct)
